0129132aa68a22b36e4f3b5b57fabe33bcdc4047
Bruno Gonçalves <brunogcp28@gmail.com>
2025-05-01 07:16:53 -0400

    Adiciona atividade sort users by age

---
+++ sort-users-by-age/index.js
@@ -0,0 +1,15 @@
+function sortByAge(arr){
+    return arr.sort((a, b) => a.age - b.age);
+}
+
+let john = { name: "John", age: 15 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let arr = [ pete, john, mary ];
+
+console.log(sortByAge(arr))
+
+console.log(arr[0].name); // John
+console.log(arr[1].name); // Mary
+console.log(arr[2].name); // Pete
